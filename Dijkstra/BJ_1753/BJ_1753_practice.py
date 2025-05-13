9ac8fe71c8d08643ade8f6d1b3c40f1e3ca4d95c
박리아 <danny6217@gmail.com>
2025-05-13 20:34:01 +0900

    practice

--- Dijkstra/BJ_1753/BJ_1753_practice.py
+++ Dijkstra/BJ_1753/BJ_1753_practice.py
@@ -1,2 +1,33 @@
 import sys
+import heapq
 
+input = sys.stdin.readline
+INF = sys.maxsize
+
+V, E = map(int, input().split())
+K = int(input())
+
+edge = [[] for _ in range(V+1)]
+for _ in range(E):
+    u, v, w = map(int, input().split())
+    edge[u].append([w, v])
+
+dist = [INF for _ in range(V+1)]
+dist[K] = 0
+
+heap = [[0, K]]
+
+while heap:
+    ew, ev = heapq.heappop(heap)
+    if dist[ev] != ew: #오답 이해 못했었음
+        continue
+    for nw, nv in edge[ev]:
+        if dist[nv] > ew + nw:
+            dist[nv] = ew + nw #오답 여기 dist[ev] = ew + nw라고 풀었었음
+            heapq.heappush(heap, [dist[nv], nv]) #오답 heapq.heappush(heap, [dist[nw], nw])라고 풀었었음
+
+for i in range(1, V+1):
+    if dist[i] == INF:
+        print("INF")
+    else:
+        print(dist[i])
