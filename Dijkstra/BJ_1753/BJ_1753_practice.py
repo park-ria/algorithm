e5d60cff2626e3f8fb269ae4827babcb36e2f3e9
박리아 <danny6217@gmail.com>
2025-05-19 20:22:54 +0900

    250519

--- Dijkstra/BJ_1753/BJ_1753_practice.py
+++ Dijkstra/BJ_1753/BJ_1753_practice.py
@@ -19,12 +19,12 @@ heap = [[0, K]]
 
 while heap:
     ew, ev = heapq.heappop(heap)
-    if dist[ev] != ew: #오답 이해 못했었음 : 다익스트라는 최소 거리부터 순차적으로 처리하므로 dist[ev] > ew가 발생하지 않음
+    if dist[ev] != ew: # 오답 이해 못했었음 : 다익스트라는 최소 거리부터 순차적으로 처리하므로 dist[ev] > ew가 발생하지 않음
         continue
     for nw, nv in edge[ev]:
         if dist[nv] > ew + nw:
-            dist[nv] = ew + nw #오답 여기 dist[ev] = ew + nw라고 풀었었음
-            heapq.heappush(heap, [dist[nv], nv]) #오답 heapq.heappush(heap, [dist[nw], nw])라고 풀었었음
+            dist[nv] = ew + nw # 오답 여기 dist[ev] = ew + nw라고 풀었었음
+            heapq.heappush(heap, [dist[nv], nv]) # 오답 heapq.heappush(heap, [dist[nw], nw])라고 풀었었음
 
 for i in range(1, V+1):
     if dist[i] == INF:
