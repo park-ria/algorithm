7ac7fac8b5b0a9bdc0b5fe5d6462766fc18618e7
박리아 <danny6217@gmail.com>
2025-07-09 18:14:48 +0900

    binary search

--- BinarySearch/BJ_10816/BJ_10816.py
+++ BinarySearch/BJ_10816/BJ_10816.py
@@ -5,19 +5,26 @@ N = int(input())
 cardList = sorted(list(map(int, input().split())))
 M = int(input())
 targetList = list(map(int, input().split()))
-resultList = [0] * M
 
-def search(start, end, target, index):
+count = {}
+for card in cardList:
+    if card in count:
+        count[card] += 1
+    else:
+        count[card] = 1
+
+def search(start, end, target):
     if start > end:
-        return
+        return 0
 
     mid = (start+end) // 2
 
     if cardList[mid] == target:
-        resultList[index] += 1
+        return count[target]
+    elif cardList[mid] < target:
+        return search(mid+1, end, target)
+    else:
+        return search(start, mid-1, target)
 
-
-for index, target in enumerate(targetList):
-    search(0, N-1, target, index)
-
-print(*resultList)
+for target in targetList:
+    print(search(0, N-1, target), end=" ")
