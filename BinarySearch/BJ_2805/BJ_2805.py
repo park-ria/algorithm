2097ea06d070bbeee5617647a9ae554083c297fe
박리아 <danny6217@gmail.com>
2025-07-17 21:24:53 +0900

    이진탐색으로 나무 높이 구하기

--- BinarySearch/BJ_2805/BJ_2805.py
+++ BinarySearch/BJ_2805/BJ_2805.py
@@ -1,7 +1,22 @@
 import sys
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
-height = list(map(int, input().split()))
-print(N, M)
-print(height)
+n, m = map(int, input().split())
+tree = list(map(int, input().split()))
+
+start, end = 0, max(tree)
+
+while start <= end:
+    sum = 0
+    mid = (start + end) // 2
+
+    for height in tree:
+        if height > mid:
+            sum += height - mid
+
+    if sum < m:
+        end = mid - 1
+    else:
+        start = mid + 1
+
+print(end)
