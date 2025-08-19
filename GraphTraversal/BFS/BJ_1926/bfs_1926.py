e380eb70ae772347ab186c1416aa74e8a2a4688e
박리아 <danny6217@gmail.com>
2025-08-19 18:21:55 +0900

    bfs

--- GraphTraversal/BFS/BJ_1926/bfs_1926.py
+++ GraphTraversal/BFS/BJ_1926/bfs_1926.py
@@ -16,9 +16,21 @@ def bfs(y, x):
         for k in range(4):
             ny = ey + dy[k]
             nx = ex + dx[k]
-            if 0<=ny<n and 0<=nx<m:
+            if 0<=ny<n and 0 <= nx < m:
                 if map[ny][nx] == 1 and visited[ny][nx] == False:
                     rs += 1
                     visited[ny][nx] = True
                     q.append((ny,nx))
     return rs
+
+cnt = 0
+maxv = 0
+for j in range(n):
+    for i in range(m):
+        if map[j][i] == 1 and visited[j][i] == False:
+            visited[j][i] = True
+            cnt += 1
+            maxv = max(maxv, bfs(j, i))
+
+print(cnt)
+print(maxv)
