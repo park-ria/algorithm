c1cf5a8519533b82492cd3985f2cb55a728c0a95
박리아 <danny6217@gmail.com>
2025-08-21 15:23:32 +0900

    2884 algorithm

--- Math/BJ_2884.py
+++ Math/BJ_2884.py
@@ -2,4 +2,11 @@ import sys
 input = sys.stdin.readline
 
 hour, minute = map(int, input().split())
-print(hour, minute)
+
+if minute < 45:
+    minute = minute + 60 - 45
+    hour -= 1
+else:
+    minute = minute - 45
+
+print((hour + 24) if hour < 0 else hour, minute)
